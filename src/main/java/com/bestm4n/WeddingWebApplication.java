cc579a199e67a6ce36b498eac10538fb4e49dc08
Sikora, Richard <richard.sikora@tieto.com>
2017-01-09 08:22:00 +0100

    Add application skeleton

---
+++ src/main/java/com/bestm4n/WeddingWebApplication.java
@@ -0,0 +1,12 @@
+package com.bestm4n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingWebApplication {
+
+	public static void main(String[] args) {
+    SpringApplication.run(WeddingWebApplication.class, args);
+	}
+}
