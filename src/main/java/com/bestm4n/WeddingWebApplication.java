9375492b132b3a44d13867a4e7795e7b29d842e3
Sikora, Richard <richard.sikora@tieto.com>
2017-01-24 09:11:59 +0100

    Add h2 embedded database support

--- src/main/java/com/bestm4n/WeddingWebApplication.java
+++ src/main/java/com/bestm4n/WeddingWebApplication.java
@@ -1,21 +1,25 @@
 package com.bestm4n;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class WeddingWebApplication {
+public class WeddingWebApplication
+{
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(WeddingWebApplication.class, args);
-	}
+  }
 
   @RequestMapping("/api/presents")
-	public String presents() {
-	  return "[{\n" +
+  public String presents() {
+    return "[{\n" +
         "  \"name\": \"Cleaner\",\n" +
         "  \"price\": 200.00\n" +
         "}, {\n" +
@@ -33,4 +37,8 @@ public class WeddingWebApplication {
         "}]";
   }
 
+  @Bean
+  ServletRegistrationBean h2servletRegistration() {
+    return new ServletRegistrationBean(new WebServlet(), "/h2/*");
+  }
 }
