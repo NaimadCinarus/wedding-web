d7090de221e06c282b76a04a837953023a9704fc
Sikora, Richard <richard.sikora@tieto.com>
2017-01-26 20:33:28 +0100

    Fix test data with proper status value

--- src/test/resources/db/data.sql
+++ src/test/resources/db/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO presents(title, price, status) values('Zlatá rybka', 150, 'AVAILABLE');
 INSERT INTO presents(title, price, status) values('Svíčky', 200, 'AVAILABLE');
-INSERT INTO presents(title, price, status) values('Lednička', 5500, 'VERIFIED');
+INSERT INTO presents(title, price, status) values('Lednička', 5500, 'VERIFYING');
 INSERT INTO presents(title, price, status) values('Kuchyňský nůž', 300, 'AVAILABLE');
 INSERT INTO presents(title, price, status) values('Tablet', 4500, 'RESERVED');
 INSERT INTO presents(title, price, status) values('Meteostanice', 400, 'AVAILABLE');
